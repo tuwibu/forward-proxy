7527c1a70f4f28ff967fce2a0ec051635f53aa40
tuwibu <tuwibu2021@gmail.com>
2023-12-26 19:51:02 +0700

    Remove build

--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.random = void 0;
-const random = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-exports.random = random;
